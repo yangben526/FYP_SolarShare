be3c6548efcb57b32adb5b08582456d5e9b8c519
yangben526 <zheng97yg@gmail.com>
2021-02-23 16:01:26 +0800

    Add files via upload

---
+++ energydatadisplaypage/js/dataenergyfunction.js
@@ -0,0 +1,11 @@
+function openenergyboughtlist(){				
+
+	window.open('http://localhost:8080/P2PTrading/energyboughtbdshowentry.php','popUpWindow','height=800,width=1100,left=100,top=100,scrollbars=no,toolbar=yes,menubar=no,location=no,directories=no,status=no,resizable=yes');		
+	
+}
+
+function openenergysoldlist(){				
+
+	window.open('http://localhost:8080/P2PTrading/energysoldbdshowentry.php','popUpWindow','height=800,width=1100,left=100,top=100,scrollbars=no,toolbar=yes,menubar=no,location=no,directories=no,status=no,resizable=yes');		
+	
+}
